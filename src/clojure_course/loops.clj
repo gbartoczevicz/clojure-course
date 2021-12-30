f48691454828c0bc8937f300c5dd73d2697b7ae8
Gabriel Bartoczevicz <gabriel.bartoczevicz@gmail.com>
2021-12-29 21:17:34 -0300

    chore: loops.clj impl

--- src/clojure_course/loops.clj
+++ src/clojure_course/loops.clj
@@ -1,1 +1,45 @@
-(ns clojure-course.loops)
+(ns clojure-course.loops
+  (:gen-class))
+
+(defn loops-loop
+  []
+  (println "Simple loop")
+
+  (loop [x 0]
+    (when (< x 10)
+      (println x)
+      (recur (inc x)))))
+
+(defn loops-dotimes
+  []
+  (println "Do-times")
+
+  (dotimes [x 10]
+    (println x)))
+
+(defn loops-whiledo
+  []
+  (println "While-do")
+
+  (def x (atom 0))
+
+  (while (< @x 10)
+    (do (println @x)
+        (swap! x inc))))
+
+(defn loops-doseq
+  []
+  (def seq [1 2 3 4 5])
+
+  (println "Do-seq")
+
+  (doseq [x seq]
+    (println "The value at the collection is" x)))
+
+(defn -main
+  "Loops in Clojure"
+  []
+  (loops-loop)
+  (loops-dotimes)
+  (loops-whiledo)
+  (loops-doseq))
